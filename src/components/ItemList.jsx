933b7ba4c003b95509032976cda89d7ecdb96650
Muhammad0073121 <mabdullah3121@gmail.com>
2025-04-13 20:51:55 +0500

    Sorter implemented

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import EmptyView from "./EmptyView";
+import Select from "react-select";
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
-  return items.length === 0 ? (
-    <EmptyView />
-  ) : (
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+
+    return;
+  });
+
+  return (
     <ul className="item-list">
-      {items.map((item) => (
+      {items.length === 0 && <EmptyView />}
+      {items.length > 0 && (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            options={sortingOptions}
+            defaultValue={sortingOptions[0]}
+          />
+        </section>
+      )}
+
+      {sortedItems.map((item) => (
         <Item
           key={item.id}
           item={item}
